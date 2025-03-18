ac8cbbc0914b2434b34a7fdcf15aeac50e907fcc
radhikajain99 <86242402+radhikajain99@users.noreply.github.com>
2025-03-18 16:38:35 +0530

    Fix UT

--- test/postProcessJob.test.ts
+++ test/postProcessJob.test.ts
@@ -4,6 +4,9 @@ import { AuthenticatorServiceMock } from "./Mocks/AuthenticatorServiceMock";
 import { TestSupport } from "./Utils/TestSupport";
 import { APIService } from "../src/services/APIService";
 import { run } from "../src/postProcessJob";
+import * as Constants from "./Constants/Constants";
+import * as EnvironmentConstants from "../src/Constants/EnvironmentConstants";
+import * as AzCliUtility from "../src/Utils/AzCliUtility";
 
 describe('post process job tests', () => {
 
@@ -20,6 +23,15 @@ describe('post process job tests', () => {
     });
 
     it("stop is called when test run is not terminated", async () => {
+        let stub = sinon.stub(AzCliUtility, "execAz");
+        let cloudShowResult = {
+            name: EnvironmentConstants.AzurePublicCloud.cloudName,
+            endpoints: {
+                resourceManager: Constants.armEndpoint,
+            }
+        };
+        // account show is not called so 1st call is cloud show
+        stub.onFirstCall().resolves(cloudShowResult);
         let stopTestRunStub = sinon.stub(APIService.prototype, "stopTestRun");
         TestSupport.setupMockForPostProcess();
 
@@ -29,6 +41,15 @@ describe('post process job tests', () => {
     });
 
     it("stop is not called when test run is terminated", async () => {
+        let stub = sinon.stub(AzCliUtility, "execAz");
+        let cloudShowResult = {
+            name: EnvironmentConstants.AzurePublicCloud.cloudName,
+            endpoints: {
+                resourceManager: Constants.armEndpoint,
+            }
+        };
+        // account show is not called so 1st call is cloud show
+        stub.onFirstCall().resolves(cloudShowResult);
         let stopTestRunStub = sinon.stub(APIService.prototype, "stopTestRun");
         TestSupport.setupMockForPostProcess(true);
 
