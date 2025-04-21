8e9dddab374b6890f9833b2f32b18b368b74db7a
Harshan B <Harshan01@gmail.com>
2025-04-21 18:28:01 +0530

    [AutoStop Max VU] Update API version to 2025-03-01-preview and add missing key test (#126)

--- test/Constants/AutoStopCriteriaYamls.ts
+++ test/Constants/AutoStopCriteriaYamls.ts
@@ -14,6 +14,23 @@ export const UndefinedMaxVUAutostop : any =
         maximumVirtualUsersPerEngine: undefined,
     }
 }
+
+export const MissingKeyMaxVUAutostop : any = 
+{
+    version: 'v0.1',
+    testId: 'SampleTest',
+    testName: 'SampleTest',
+    displayName: 'Sample Test',
+    description: 'Load test website home page',
+    testPlan: 'SampleTest.jmx',
+    testType: 'JMX',
+    engineInstances: 2,
+    autoStop: {
+        errorPercentage: 80,
+        timeWindow: 60,
+    }
+}
+
 export const NullMaxVUAutostop : any = 
 {
     version: 'v0.1',
