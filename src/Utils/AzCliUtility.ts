c6bfcebb81d0c4f1e02be0c45eed965ec051612f
Mohit Pavan Kumar Gadamsetty <112494281+mohitpavan@users.noreply.github.com>
2025-11-20 07:32:12 +0530

    Refactor execAz function to accept token scope

--- src/Utils/AzCliUtility.ts
+++ src/Utils/AzCliUtility.ts
@@ -1,9 +1,21 @@
 import { execFile } from "child_process";
+import { ControlPlaneTokenScope, DataPlaneTokenScope } from "../models/TaskParameters";
 
-export async function execAz(cmdArguments: string[]): Promise<any> {
+export async function execAz(tokenScope: ControlPlaneTokenScope| DataPlaneTokenScope): Promise<any> {
+    const cmdArguments = ["account", "get-access-token", "--resource"];
+    cmdArguments.push(tokenScope);
+    return runCommand(cmdArguments);
+}
+
+export async function getSubscriptions(): Promise<any> {
+    const cmdArguments = ["cloud", "show"];
+    return runCommand(cmdArguments);
+}
+
+async function runCommand(cmdArguments: string[]): Promise<any> {
     const azCmd = process.platform === "win32" ? "az.cmd" : "az";
     return new Promise<any>((resolve, reject) => {
-        execFile(azCmd, [...cmdArguments, "--out", "json"], { encoding: "utf8", shell : true }, (error:any, stdout:any) => {
+        execFile(azCmd, [...cmdArguments, "--out", "json"], { encoding: "utf8", shell : false }, (error:any, stdout:any) => {
             if (error) {
                 return reject(error);
             }
