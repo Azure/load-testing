4fa0cc45e1d5d53983cb2aad6c67dd4e11544c19
Mohit Pavan Kumar Gadamsetty <112494281+mohitpavan@users.noreply.github.com>
2025-11-20 07:31:23 +0530

    Refactor token scopes in TaskParameters interface

--- src/models/TaskParameters.ts
+++ src/models/TaskParameters.ts
@@ -1,9 +1,16 @@
+export const publicTokenScope = "https://cnt-prod.loadtesting.azure.com";
+export const usGovernmentTokenScope = "https://cnt-prod.loadtesting.azure.us";
+export const armPublicTokenScope = "https://management.core.windows.net";
+export const armUsGovernmentTokenScope = "https://management.usgovcloudapi.net";
+export type ControlPlaneTokenScope = typeof armPublicTokenScope | typeof armUsGovernmentTokenScope;
+export type DataPlaneTokenScope = typeof publicTokenScope | typeof usGovernmentTokenScope;
+
 export interface TaskParameters {
     subscriptionId: string;
     subscriptionName: string;
     environment: string;
-    armTokenScope: string;
-    dataPlaneTokenScope: string;
+    armTokenScope: ControlPlaneTokenScope;
+    dataPlaneTokenScope: DataPlaneTokenScope;
     resourceId: string;
     armEndpoint?: string;
 }
